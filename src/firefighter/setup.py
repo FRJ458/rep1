d81d4a744980b9a06a6e77cced028a5379c3a27c
Mikhail Fadin <fadin.edu@gmail.com>
2026-01-14 10:05:38 +0200

    Add entry points for firefighter and movement scripts

--- src/firefighter/setup.py
+++ src/firefighter/setup.py
@@ -6,11 +6,12 @@ setup(
     name=package_name,
     version='1.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('lib/' + package_name, ['firefighter/firefighter.py']), ('lib/' + package_name, ['firefighter/movement.py'])
-    ],
+    entry_points={
+        'console_scripts': [
+            'firefighter = firefighter.firefighter:main',
+            'movement = firefighter.movement:main',
+        ],
+    },
     install_requires=['setuptools'],
     zip_safe=True,
     description='Firefighter bot',
