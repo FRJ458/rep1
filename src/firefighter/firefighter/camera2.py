4e2446cbabea2c749485168f0854a2ca709bad38
ii <frj0458@gmail.com>
2026-01-15 14:29:28 +0200

    color values changed slightly

--- src/firefighter/firefighter/camera2.py
+++ src/firefighter/firefighter/camera2.py
@@ -116,9 +116,9 @@ class ImageNode(Node):
 
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-        lower_red1 = np.array([0, 100, 100])
-        upper_red1 = np.array([10, 255, 255])
-        lower_red2 = np.array([160, 100, 100])
+        lower_red1 = np.array([0, 140, 120])
+        upper_red1 = np.array([7, 255, 255])
+        lower_red2 = np.array([165, 140, 120])
         upper_red2 = np.array([179, 255, 255])
 
         mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
